8ea8c48ff58f8dd230ee80b30960ef9b16c91e53
kevinjang <kevinjang@163.com>
2020-01-19 15:37:42 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Sidebar, Segment, Menu, Icon, Header, Image, Breadcrumb } from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar.Pushable as={Segment}>
+        <Sidebar as={Menu} animation={'overlay'}
+          icon={'labeled'} inverted vertical
+          width={'thin'}>
+          <Menu.Item as={'a'}>
+            <Icon name={'home'}></Icon>
+            Home
+            </Menu.Item>
+          <Menu.Item as={'a'}>
+            <Icon name={'gamepad'}></Icon>
+            Games
+            </Menu.Item>
+          <Menu.Item as={'a'}>
+            <Icon name={'camera'}></Icon>
+            Channels
+            </Menu.Item>
+        </Sidebar>
+        <Sidebar.Pusher>
+          <Segment basic>
+            <Header as={'h3'}>Application Content</Header>
+            <Image src="/images/wireframe/paragraph.png"></Image>
+          </Segment>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+      <Button primary>点我</Button>
+      <div>
+        <Breadcrumb>
+          <Breadcrumb.Section href="/home">Home</Breadcrumb.Section>
+          <Breadcrumb.Divider content={'.../'}/>
+          <Breadcrumb.Section href="/store">Store</Breadcrumb.Section>
+          <Breadcrumb.Divider content={'.../'}/>
+          <Breadcrumb.Section active>T-Shirt</Breadcrumb.Section>
+        </Breadcrumb>
+      </div>
     </div>
   );
 }
